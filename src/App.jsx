17dd9e88f46721a561f913afd36355720b815f7c
Rene Pacifique Mutanguha <PACIFIQUEGIT>
2024-11-12 13:47:33 +0200

    commits of today

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
-import {Home} from './components/Home'; // Make sure to create these pages
-import {About} from './components/About';
-import {Contact} from './components/Contact';
-import {Donate} from './components/Donate';
+import { Home } from './components/Home'; // Make sure to create these pages
+import { About } from './components/About';
+import { Contact } from './components/Contact';
+import { Donate } from './components/Donate';
 import { Footer } from './components/Footer';
 import { Payment } from './components/Payment';
 import { Card } from './components/Card';
-import { Volunteer } from './components/volunteer';
+import { Volunteer } from './components/Volunteer';
 
 function App() {
   return (
@@ -19,14 +19,14 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/donate" element={<Donate />} />
-        <Route path="/payment" element={<Payment />} >
-          <Route path='card' element={<Card />} />
+        {/* Corrected the path for the Card route */}
+        <Route path="/payment" element={<Payment />}>
+          <Route path="card" element={<Card />} />
         </Route>
         <Route path="/volunteer" element={<Volunteer />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
-    
   );
 }
 
