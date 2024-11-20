f280de6ba8b7155a6659985a85936ea474119a0c
baudoin <baudoinvicbolingo@gmail.com>
2024-11-20 16:00:31 +0200

    start fixing payment page

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -14,44 +14,14 @@ export const Payment = () => {
 
   return (
     <>
-      <div className="bg-gray-900 text-white p-4 shadow-md">
-        <div className="flex items-center justify-between max-w-6xl mx-auto">
-          <div className="text-2xl font-bold">
-            <span className="text-yellow-500">Charity</span>
-          </div>
-          <div className="flex space-x-6">
-            <Link className="hover:text-yellow-500 transition-colors py-3">Home</Link>
-            <Link className="hover:text-yellow-500 transition-colors py-3">About</Link>
-            <Link className="hover:text-yellow-500 transition-colors py-3">Contact</Link>
-            <Link className="hover:text-yellow-500 transition-colors bg-green-500 text-white px-5 py-3 rounded-lg">Donate</Link>
-          </div>
-        </div>
-      </div>
+      
 
-      <div className="flex justify-center bg-gray-300 p-6">
-        <div className="w-full max-w-3xl text-center">
+      <div className="flex justify-center  p-6">
+        <div className="w-full max-w-3xl text-center mt-32">
           {/* Custom Dropdown for Payment Method */}
           <h1 className='font-bold text-[30px] mb-4'>Payment Details</h1>
           <div className='shadow-lg bg-white p-5 mb-5'>
-          <div className="space-y-3 mb-5">
-            <div className="relative">
-              <button 
-                onClick={toggleDropdown} 
-                className="py-2 rounded-lg  focus:outline-none">
-                <div className='flex'>Select Payment Method <IoIosArrowDown className='mt-2' /></div>
-              </button>
-
-              {/* Dropdown menu */}
-              {isOpen && (
-                <div className="absolute top-0 left-[64%] bg-white shadow-lg rounded-lg">
-                  <div className="py-2">
-                    <Link onClick={toggleDropdown}  to="card" className="block py-2 text-blue-500 hover:bg-gray-200">Pay with Card</Link>
-                    <Link onClick={toggleDropdown}  to="paypal" className="block py-2 text-blue-500 hover:bg-gray-200">Pay with PayPal</Link>
-                  </div>
-                </div>
-              )}
-            </div>
-          </div>
+         
 
           {/* Payment Card Section */}
           <div className="h-[300px] w-full max-w-md mx-auto p-4 mb-4">
